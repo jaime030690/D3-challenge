e499965f43cbde8265df212518142ba99c1252af
Jaime Echevarria <jaime030690@gmail.com>
2020-04-04 19:13:47 -0400

    read csv file

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,11 @@
-// @TODO: YOUR CODE HERE!
+var csv_file = "assets/data/data.csv";
+
+// read in csv file
+d3.csv(csv_file).then(function(data) {
+    console.log(data);
+
+    var smokers = data.map(item => +item.smokes);
+    var age = data.map(item => +item.age);
+
+
+}).catch(error => console.log(error));
